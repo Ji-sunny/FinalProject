753ac97b6b211da326e8cfe7bf29311414d67b9b
SeungtaeKim <rlatmdxo50@gmail.com>
2021-06-03 11:52:11 +0900

    이전 작업까지

---
+++ static/js/litepicker.js
@@ -0,0 +1,72 @@
+var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+const picker = new Litepicker({
+    element: document.getElementById('litepicker'),
+    autoApply: false,
+    singleMode: false,
+    inlineMode: true, // 항상 열려있게
+    lang: "ko-kr", // 언어 설정
+    maxDays: 365, // 최대 선택 일자 수
+    // scrollToDate: true,
+    // showTooltip: true,
+    // autoRefresh: true
+    setup: (picker) => {
+        picker.on('button:apply', (date1, date2) => {
+            date_1 = date1.dateInstance.toString().substr(4,21)
+            date_2 = date2.dateInstance.toString().substr(4,21)
+            var d1s = date_1.split(' ')
+            var d2s = date_2.split(' ')
+            d1s_m = d1s[0]
+            d2s_m = d2s[0]
+            var step;
+            for (step = 0; step < 12; step++) {
+                if (months[step] == d1s_m) {
+                    d1s_m = (step+1)
+                }
+                if (months[step] == d2s_m) {
+                    d2s_m = (step+1)
+                }
+            }
+            if (d1s_m<10) {
+                d1s_m = "0" + d1s_m.toString()
+            }
+            if (d2s_m<10) {
+                d2s_m = "0" + d2s_m.toString()
+            }
+
+            console.log(d1s[2] + '-' + d1s_m + '-' + d1s[1] + " 00:00:00")
+            console.log(d2s[2] + '-' + d2s_m + '-' + d2s[1] + " 23:00:00")
+
+            start_date = d1s[2] + '-' + d1s_m + '-' + d1s[1] + " 00:00:00"
+            end_date = d2s[2] + '-' + d2s_m + '-' + d2s[1] + " 23:00:00"
+        })
+    }
+});
+
+$("#button-apply").click(function (){
+    console.log("start: " + starttime);
+    console.log("end: " + endtime);
+        if(starttime != '' && endtime != '') {
+        if(starttime < endtime) {
+            $.ajax({
+                type : 'POST',
+                url : '/item_request',
+                data : {
+                    fromdate:starttime,
+                    todate:endtime
+                },
+                dataType : 'text',
+                success : function(result){
+                    alert("result = "+ result);
+                },
+                error : function(xtr,status,error){
+                    alert(xtr +":"+status+":"+error);
+                }
+            });
+        }else {
+            alert("Please Check the Date");
+        }
+    } else {
+        alert("Please Select the Date");
+    }
+});
