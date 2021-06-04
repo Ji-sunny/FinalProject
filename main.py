e8ca052669943065f2a6edefb7a4e6ca2d8cdc44
SeungtaeKim <rlatmdxo50@gmail.com>
2021-06-04 14:47:33 +0900

    당진,울산 에너지까지 완료한거 다시 올림!

--- main.py
+++ main.py
@@ -74,16 +74,19 @@ def ajax():
     column = data['column']
     if '당진' in location:
         data = get_dangjin(start, end, column)
+        energy_data = get_dangjin_energy(start, end, location)
     elif '울산' in location:
         data = get_ulsan(start, end, column)
-
+        energy_data = get_ulsan_energy(start, end, location)
     # print(data)
     chart_data = []
+    energy_chart_data = []
     # date: new Date(2018, 0, i), open: open, close: close
 
     for row in data.itertuples():
         chart_data.append({"date":row[1].to_pydatetime(), "open":row[2], "close":row[3]})
-
+    for row in energy_data.itertuples():
+        energy_chart_data.append({"date":row[1].to_pydatetime(), "open":row[2], "close":row[3]})
     print("--" * 10)
     return jsonify(chart_data)
 
@@ -105,8 +108,42 @@ def get_dangjin(start, end, column):
 
 
 def get_ulsan(start, end, column):
-    data = ""
+    if '온도' in column:
+        column = 'temperature'
+    elif '습도' in column:
+        column = 'humidity'
+    elif '일조' in column:
+        column = 'sunshinehour'
+    else:
+        column = 'cloud'
+    sql = """select obs.timedate, obs.{2} , fc.{2}
+                    from ulsan_obs obs join ulsan_fcst fc on obs.timedate = fc.timedate
+                    where obs.timedate between '{0}' AND '{1}'""".format(start, end, column)
+    data = oracle_db.read_sql(sql)
     return data
+def get_dangjin_energy(start, end, location):
+    if '당진수상 태양광' in location:
+        location = 'dangjin_floating'
+    elif '당진자재창고태양광' in location:
+        location = 'dangjin_warehouse'
+    elif '당진태양광' in location:
+        location = 'dangjin'
+    sql = """select obs.timedate, obs.{2} , fc.{2}
+                    from energy_obs obs join energy_fcst fc on obs.timedate = fc.timedate
+                    where obs.timedate between '{0}' AND '{1}'""".format(start, end, location)
+    energy_data = oracle_db.read_sql(sql)
+    return energy_data
+
+
+def get_ulsan_energy(start, end, location):
+    if '울산태양광' in location:
+        location = 'ulsan'
+    sql = """select obs.timedate, obs.{2} , fc.{2}
+                    from energy_obs obs join energy_fcst fc on obs.timedate = fc.timedate
+                    where obs.timedate between '{0}' AND '{1}'""".format(start, end, location)
+
+    energy_data = oracle_db.read_sql(sql)
+    return energy_data
 
 
 if __name__ == "__main__":
